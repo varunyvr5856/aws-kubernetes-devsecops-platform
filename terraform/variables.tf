e1c385a0f1366352335c08a8b8e9bb7f8aecbda2
Varun Reddy <varunreddy5856@gmail.com>
2026-08-27 01:19:42 +1000

    feat: add public and private subnet CIDRs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,4 +33,14 @@ variable "availability_zones" {
   default     = ["ap-southeast-2a", "ap-southeast-2b"]
 }
 
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for the public subnets."
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
 
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for the private subnets."
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
