10e67e30afe1ea35181012477b61de9e21b92e10
Varun Reddy <varunreddy5856@gmail.com>
2026-08-28 23:02:01 +1000

    added destination

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,8 +24,9 @@ resource "aws_route_table" "public" {
   })
 }
 
-resource "aws_route" "public_internet_access" {
+resource "aws_route" "public_internet" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
+
