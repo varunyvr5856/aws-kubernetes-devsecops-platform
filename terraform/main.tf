7c178bf1993bec2f609693c6199266f8f6e38fa0
Varun Reddy <varunreddy5856@gmail.com>
2026-08-30 00:01:09 +1000

    Remove NAT EIPs until NAT gateway is required

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,3 +70,11 @@ resource "aws_route_table" "private" {
     Tier = "private"
   })
 }
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+
+
