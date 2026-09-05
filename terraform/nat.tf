75a47fb4e49b76fa8a1549ce6708b1f6049d586e
Varun Reddy <varunreddy5856@gmail.com>
2026-09-05 21:33:09 +1000

    Added NATs in 2 public subnets for private subnets for high availability

--- terraform/nat.tf
+++ terraform/nat.tf
@@ -1,4 +1,5 @@
 resource "aws_eip" "nat" {
+  count = length(aws_subnet.public)
   domain = "vpc"
 
   tags = merge(local.common_tags, {
@@ -7,8 +8,10 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
+  
+  count = length(aws_subnet.public)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-nat"
@@ -24,6 +27,6 @@ resource "aws_route" "private_nat" {
 
   route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.main.id
+  nat_gateway_id         = aws_nat_gateway.main[count.index].id
 }
 
