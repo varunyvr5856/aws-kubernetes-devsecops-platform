87df77f0bff9f6ac2fab1aeb99fd451f2e0baa96
Varun Reddy <varunreddy5856@gmail.com>
2026-09-09 21:46:06 +1000

    Configure S3 backend for Terraform remote state

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+        bucket = "varun-terraform-state-900053548419"
+        key   = "aws-kubernetes-devsecops-platform/dev/terraform.tfstate"
+        region = "ap-southeast-2"
+        encrypt = true
+        use_lockfile = true
+    }
+}
